e5b28329a1cfa4fcb2f3ec8570e54d0ff150483b
1xInstinct <antoniomachadomaliketh@gmail.com>
2024-11-21 23:56:51 -0300

    Initial commit

---
+++ src/main/java/com/maliketh/TechGearAPI/TechGearApiApplication.java
@@ -0,0 +1,13 @@
+package com.maliketh.TechGearAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechGearApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechGearApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maliketh/TechGearAPI/TechGearApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maliketh.TechGearAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechGearApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
