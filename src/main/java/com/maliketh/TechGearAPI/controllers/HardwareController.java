c889d668f192b811d715c442796a902339594aed
1xInstinct <antoniomachadomaliketh@gmail.com>
2024-12-06 17:45:43 -0300

    Implemented Delete method

--- src/main/java/com/maliketh/TechGearAPI/controllers/HardwareController.java
+++ src/main/java/com/maliketh/TechGearAPI/controllers/HardwareController.java
@@ -12,7 +12,9 @@ import jakarta.validation.Valid;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,5 +44,11 @@ public class HardwareController {
     public void atualizar(@RequestBody @Valid DadosAtualizarHardware dados) {
         var hardware = repository.getReferenceById(dados.id());
         hardware.atualizarinformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
--- src/main/java/com/maliketh/TechGearAPI/hardware/HardwareRepository.java
+++ src/main/java/com/maliketh/TechGearAPI/hardware/HardwareRepository.java
@@ -3,4 +3,5 @@ package com.maliketh.TechGearAPI.hardware;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HardwareRepository extends JpaRepository<Hardware, Long> {
+    
 }
