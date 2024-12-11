3197e47cd1100252954abb51899f28e9c676f3af
1xInstinct <antoniomachadomaliketh@gmail.com>
2024-12-10 21:52:35 -0300

    Added error handler.

--- src/main/java/com/maliketh/TechGearAPI/infra/ErrorHandler.java
+++ src/main/java/com/maliketh/TechGearAPI/infra/ErrorHandler.java
@@ -1,5 +1,16 @@
 package com.maliketh.TechGearAPI.infra;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
 public class ErrorHandler {
 
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handler404 () {
+        return ResponseEntity.notFound().build();
+    }
 }
