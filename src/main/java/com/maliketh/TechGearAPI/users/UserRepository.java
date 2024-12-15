34bcc38356bce1fb4b112adc798e325667b27c82
1xInstinct <antoniomachadomaliketh@gmail.com>
2024-12-14 21:39:42 -0300

    User repository and authentication service added.

---
+++ src/main/java/com/maliketh/TechGearAPI/users/UserRepository.java
@@ -0,0 +1,10 @@
+package com.maliketh.TechGearAPI.users;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByLogin(String login);
+
+}
